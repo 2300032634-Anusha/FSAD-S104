be470dfd17c802cf294de7b445dc8ffce2a9b51d
2300032634-Anusha <2300032634@kluniversity.in>
2025-08-09 23:54:15 +0530

    3rd commit

--- src/App.js
+++ src/App.js
@@ -5,7 +5,7 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        GITHUB Pages
+        GITHUB Pages Deployment
       </header>
     </div>
   );
